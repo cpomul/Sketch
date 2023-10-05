d9af9cdd97d8980c5243c3242f0d75e4b2987a62
Tudor Mihai Adrian <tudor.mihai.adi00@gmail.com>
2023-10-05 14:02:37 +0300

    Add 'click' painting

--- script.js
+++ script.js
@@ -1,14 +1,34 @@
 const grid = document.querySelector("#grid");
 
-for (let i = 0; i < 1000; i++) {
-  let square = document.createElement("div");
-  square.style.cssText = "width: 16px; height: 16px; background-color: black;";
-  grid.appendChild(square);
-
-  square.addEventListener("mouseover", function () {
-    square.style.backgroundColor = "white";
-  });
-  square.addEventListener("mouseout", function () {
-    square.style.backgroundColor = "black";
+function hoverColor(square) {
+  square.addEventListener("mouseover", () => {
+    square.style.cssText = "background-color: white";
   });
 }
+
+function revertColor(square) {
+  square.addEventListener("mouseleave", () => {
+    square.style.cssText = "background-color: black";
+  });
+}
+
+function changeColor(square) {
+  square.addEventListener("click", () => {
+    square.style.cssText = "background-color: green";
+  });
+}
+
+function generateGrid(squareNumbers) {
+  for (let i = 0; i < squareNumbers; i++) {
+    square = document.createElement("div");
+    square.style.cssText =
+      "width: 16px; height: 16px; background-color: black;";
+    grid.appendChild(square);
+
+    changeColor(square);
+    //hoverColor(square);
+    //revertColor(square);
+  }
+}
+
+generateGrid(500);
